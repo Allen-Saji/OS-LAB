36e2bd289663f807ad579cc80347dadf8d21263a
Allen Saji <83496985+Allen-Saji@users.noreply.github.com>
2022-07-27 16:22:43 +0530

    Create fcfs_disk.c

---
+++ fcfs_disk.c
@@ -0,0 +1,44 @@
+// C++ program to demonstrate
+// FCFS Disk Scheduling algorithm
+
+#include <stdio.h>
+
+int size = 8;
+
+void FCFS(int arr[], int head)
+{
+	int seek_count = 0;
+	int distance, cur_track;
+
+	for (int i = 0; i < size; i++) {
+		cur_track = arr[i];
+
+		distance = cur_track - head;
+
+		seek_count += distance;
+
+		head = cur_track;
+	}
+
+	printf("Total number of seek operations = %d", seek_count);
+		
+	printf( "Seek Sequence is: \n");
+
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+
+int main()
+{
+
+	
+	int arr[] = { 176, 79, 34, 60, 92, 11, 41, 114 };
+	int head = 50;
+
+	FCFS(arr, head);
+
+	return 0;
+}
