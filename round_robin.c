e935661ef6238f547b9980afb47ec2b190cd5c92
Allen Saji <83496985+Allen-Saji@users.noreply.github.com>
2022-10-21 06:17:24 +0530

    Update round_robin.c

--- round_robin.c
+++ round_robin.c
@@ -1,125 +1,96 @@
-// C++ program for implementation of RR scheduling
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
-// Function to find the waiting time for all
-// processes
-void findWaitingTime(int processes[], int n,
-			int bt[], int wt[], int quantum)
+struct times
 {
-	// Make a copy of burst times bt[] to store remaining
-	// burst times.
-	int rem_bt[n];
-	for (int i = 0 ; i < n ; i++)
-		rem_bt[i] = bt[i];
+    int p, art, but, wtt, tat, rnt;
+};
 
-	int t = 0; // Current time
-
-	// Keep traversing processes in round robin manner
-	// until all of them are not done.
-	while (1)
-	{
-		bool done = true;
-
-		// Traverse all processes one by one repeatedly
-		for (int i = 0 ; i < n; i++)
-		{
-			// If burst time of a process is greater than 0
-			// then only need to process further
-			if (rem_bt[i] > 0)
-			{
-				done = false; // There is a pending process
-
-				if (rem_bt[i] > quantum)
-				{
-					// Increase the value of t i.e. shows
-					// how much time a process has been processed
-					t += quantum;
-
-					// Decrease the burst_time of current process
-					// by quantum
-					rem_bt[i] -= quantum;
-				}
-
-				// If burst time is smaller than or equal to
-				// quantum. Last cycle for this process
-				else
-				{
-					// Increase the value of t i.e. shows
-					// how much time a process has been processed
-					t = t + rem_bt[i];
-
-					// Waiting time is current time minus time
-					// used by this process
-					wt[i] = t - bt[i];
-
-					// As the process gets fully executed
-					// make its remaining burst time = 0
-					rem_bt[i] = 0;
-				}
-			}
-		}
-
-		// If all processes are done
-		if (done == true)
-		break;
-	}
+void sortart(struct times a[], int pro)
+{
+    int i, j;
+    struct times temp;
+    for (i = 0; i < pro; i++)
+    {
+        for (j = i + 1; j < pro; j++)
+        {
+            if (a[i].art > a[j].art)
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    return;
 }
 
-// Function to calculate turn around time
-void findTurnAroundTime(int processes[], int n,
-						int bt[], int wt[], int tat[])
-{
-	// calculating turnaround time by adding
-	// bt[i] + wt[i]
-	for (int i = 0; i < n ; i++)
-		tat[i] = bt[i] + wt[i];
-}
-
-// Function to calculate average time
-void findavgTime(int processes[], int n, int bt[],
-									int quantum)
-{
-	int wt[n], tat[n], total_wt = 0, total_tat = 0;
-
-	// Function to find waiting time of all processes
-	findWaitingTime(processes, n, bt, wt, quantum);
-
-	// Function to find turn around time for all processes
-	findTurnAroundTime(processes, n, bt, wt, tat);
-
-	// Display processes along with all details
-	cout << "PN\t "<< " \tBT "
-		<< " WT " << " \tTAT\n";
-
-	// Calculate total waiting time and total turn
-	// around time
-	for (int i=0; i<n; i++)
-	{
-		total_wt = total_wt + wt[i];
-		total_tat = total_tat + tat[i];
-		cout << " " << i+1 << "\t\t" << bt[i] <<"\t "
-			<< wt[i] <<"\t\t " << tat[i] <<endl;
-	}
-
-	cout << "Average waiting time = "
-		<< (float)total_wt / (float)n;
-	cout << "\nAverage turn around time = "
-		<< (float)total_tat / (float)n;
-}
-
-// Driver code
 int main()
 {
-	// process id's
-	int processes[] = { 1, 2, 3};
-	int n = sizeof processes / sizeof processes[0];
-
-	// Burst time of all processes
-	int burst_time[] = {10, 5, 8};
-
-	// Time quantum
-	int quantum = 2;
-	findavgTime(processes, n, burst_time, quantum);
-	return 0;
+    int i, j, pro, time, remain, flag = 0, ts;
+    struct times a[100];
+    float avgwt = 0, avgtt = 0;
+    printf("Round Robin Scheduling Algorithm\n");
+    printf("Enter Number Of Processes : ");
+    scanf("%d", &pro);
+    remain = pro;
+    for (i = 0; i < pro; i++)
+    {
+        printf("Enter arrival time and Burst time for Process P%d : ", i);
+        scanf("%d%d", &a[i].art, &a[i].but);
+        a[i].p = i;
+        a[i].rnt = a[i].but;
+    }
+    sortart(a, pro);
+    printf("Enter Time Slice OR Quantum Number : ");
+    scanf("%d", &ts);
+    printf("\n***************************************\n");
+    printf("Gantt Chart\n");
+    printf("0");
+    for (time = 0, i = 0; remain != 0;)
+    {
+        if (a[i].rnt <= ts && a[i].rnt > 0)
+        {
+            time = time + a[i].rnt;
+            printf(" -> [P%d] <- %d", a[i].p, time);
+            a[i].rnt = 0;
+            flag = 1;
+        }
+        else if (a[i].rnt > 0)
+        {
+            a[i].rnt = a[i].rnt - ts;
+            time = time + ts;
+            printf(" -> [P%d] <- %d", a[i].p, time);
+        }
+        if (a[i].rnt == 0 && flag == 1)
+        {
+            remain--;
+            a[i].tat = time - a[i].art;
+            a[i].wtt = time - a[i].art - a[i].but;
+            avgwt = avgwt + time - a[i].art - a[i].but;
+            avgtt = avgtt + time - a[i].art;
+            flag = 0;
+        }
+        if (i == pro - 1)
+            i = 0;
+        else if (a[i + 1].art <= time)
+            i++;
+        else
+            i = 0;
+    }
+    printf("\n\n");
+    printf("***************************************\n");
+    printf("Pro\tArTi\tBuTi\tTaTi\tWtTi\n");
+    printf("***************************************\n");
+    for (i = 0; i < pro; i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\n", a[i].p, a[i].art, a[i].but, a[i].tat, a[i].wtt);
+    }
+    printf("***************************************\n");
+    avgwt = avgwt / pro;
+    avgtt = avgtt / pro;
+    printf("Average Waiting Time : %.2f\n", avgwt);
+    printf("Average Turnaround Time : %.2f\n", avgtt);
+    return 0;
 }
