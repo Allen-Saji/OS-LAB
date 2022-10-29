1ea01867fd09b88c3984498b639d04aad22fd184
Allen Saji <83496985+Allen-Saji@users.noreply.github.com>
2022-10-29 21:55:16 +0530

    Update bankers_algo.c

--- bankers_algo.c
+++ bankers_algo.c
@@ -6,27 +6,38 @@ int main()
     // P0, P1, P2, P3, P4 are the Process names here
  
     int n, m, i, j, k;
-    n = 5; // Number of processes
-    m = 3; // Number of resources
-    int alloc[5][3] = { { 0, 1, 0 }, // P0    // Allocation Matrix
-                        { 2, 0, 0 }, // P1
-                        { 3, 0, 2 }, // P2
-                        { 2, 1, 1 }, // P3
-                        { 0, 0, 2 } }; // P4
- 
-    int max[5][3] = { { 7, 5, 3 }, // P0    // MAX Matrix
-                      { 3, 2, 2 }, // P1
-                      { 9, 0, 2 }, // P2
-                      { 2, 2, 2 }, // P3
-                      { 4, 3, 3 } }; // P4
- 
-    int avail[3] = { 3, 3, 2 }; // Available Resources
+    
+    printf("Enter the number of processes: ");
+    scanf("%d",&n);
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
+   
+    int alloc[n][m], int max[n][m], int avail[m], int need[n][m];
+    
+    printf("Enter the allocation matrix:\n");
+    for(i=0;i<n;i++){
+        for(int j=0;j<m;i++){
+            scanf("%d",&alloc[i][j]);
+        }
+    }
+    
+     printf("Enter the max matrix:\n");
+    for(i=0;i<n;i++){
+        for(int j=0;j<m;i++){
+            scanf("%d",&max[i][j]);
+        }
+    }
+    
+    printf("Enter the available resources:\n");
+    for(i=0;i<m;i++){
+        scanf("%d",&avail[m]);
+    }
  
     int f[n], ans[n], ind = 0;
     for (k = 0; k < n; k++) {
         f[k] = 0;
     }
-    int need[n][m];
+    
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++)
             need[i][j] = max[i][j] - alloc[i][j];
@@ -75,7 +86,7 @@ int main()
     }
      
  
-    return (0);
+    return 0;
  
   
 }
