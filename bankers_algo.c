f5ec6c56aa5c5b1a8f3c73a9e2de3cdcdf8db825
Allen Saji <83496985+Allen-Saji@users.noreply.github.com>
2022-11-04 10:23:54 +0530

    Update bankers_algo.c

--- bankers_algo.c
+++ bankers_algo.c
@@ -1,52 +1,49 @@
+#include<stdio.h>
 
-// Banker's Algorithm
-#include <stdio.h>
-int main()
-{
-    int n, m, i, j, k;
-    
-    printf("Enter the number of processes: ");
-    scanf("%d",&n);
-    printf("Enter the number of resources: ");
-    scanf("%d", &m);
-   
-    int alloc[n][m], int max[n][m], int avail[m], int need[n][m];
-    
-    printf("Enter the allocation matrix:\n");
-    for(i=0;i<n;i++){
-        for(int j=0;j<m;i++){
-            scanf("%d",&alloc[i][j]);
-        }
+int main(){
+	int n,m;
+	printf("Enter the number of processes: ");
+	scanf("%d",&n);
+	printf("Enter the number of resources: ");
+	scanf("%d",&m);
+	
+	int alloc[n][m], max[n][m], need[n][m], avail[m];
+	
+	printf("Enter the allocation matrix:\n");
+	for(int i=0;i<n;i++){
+	   for(int j=0;j<m;j++){
+		scanf("%d",&alloc[i][j]);
+	   }
+	}
+	
+	printf("Enter the max matrix:\n");
+	for(int i=0;i<n;i++){
+	   for(int j=0;j<m;j++){
+		scanf("%d",&max[i][j]);
+	   }
+	}
+	
+	printf("Enter the available matrix:\n");
+	for(int i=0;i<m;i++){
+	    scanf("%d",&avail[i]);
+	}
+	
+	for (int i = 0; i < n; i++) {
+          for (int j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
     }
     
-     printf("Enter the max matrix:\n");
-    for(i=0;i<n;i++){
-        for(int j=0;j<m;i++){
-            scanf("%d",&max[i][j]);
-        }
-    }
-    
-    printf("Enter the available resources:\n");
-    for(i=0;i<m;i++){
-        scanf("%d",&avail[m]);
-    }
- 
-    int f[n], ans[n], ind = 0;
-    for (k = 0; k < n; k++) {
+     int f[n], ans[n], ind = 0;
+    for (int k = 0; k < n; k++) {
         f[k] = 0;
     }
     
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++)
-            need[i][j] = max[i][j] - alloc[i][j];
-    }
-    int y = 0;
-    for (k = 0; k < 5; k++) {
-        for (i = 0; i < n; i++) {
-            if (f[i] == 0) {
- 
-                int flag = 0;
-                for (j = 0; j < m; j++) {
+    for(int k=0;k<n;k++){
+    	for(int i=0;i<n;i++){
+    		if(f[i]==0){
+    			int flag = 0;
+    			
+    			for (int j = 0; j < m; j++) {
                     if (need[i][j] > avail[j]){
                         flag = 1;
                          break;
@@ -55,15 +52,15 @@ int main()
  
                 if (flag == 0) {
                     ans[ind++] = i;
-                    for (y = 0; y < m; y++)
+                    for (int y = 0; y < m; y++)
                         avail[y] += alloc[i][y];
                     f[i] = 1;
                 }
-            }
-        }
+    	   }
+    	}
     }
-   
-      int flag = 1;
+    
+    int flag = 1;
        
       for(int i=0;i<n;i++)
     {
@@ -78,13 +75,11 @@ int main()
       if(flag==1)
     {
       printf("Following is the SAFE Sequence\n");
-      for (i = 0; i < n - 1; i++)
+      for (int i = 0; i < n - 1; i++)
         printf(" P%d ->", ans[i]);
       printf(" P%d", ans[n - 1]);
     }
-     
- 
-    return 0;
- 
-  
+    
+    printf("\n");
+	return 0;
 }
